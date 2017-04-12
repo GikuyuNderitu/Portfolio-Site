0efeba03f5d92e0847a8870e16409eeb4edbf167
Gikuyu Nderitu <gikyunderitu@gmail.com>
2017-04-11 21:05:46 -0400

    Update angular module. Begin to implement angular material with sidenav implementation

--- client/assets/js/controllers/portfoliosController.js
+++ client/assets/js/controllers/portfoliosController.js
@@ -1,6 +1,14 @@
-app.controller('portfoliosController', ['$scope', 'portfolioFactory', function($scope, portfolioFactory){
-	$scope.obj = {}
+app.controller('portfoliosController', ['$scope', 'portfolioFactory', '$mdSidenav', function($scope, portfolioFactory, $mdSidenav){
 	$scope.objs = []
+
+	$scope.toggleSideNav = function () {
+		console.log('hello');
+		$mdSidenav('side-navigator')
+		.toggle()
+		.then(() => {
+			console.log('toggle complete');
+		})
+	}
 
 	$scope.func = function (idx) {
 		portfolioFactory.delete( () => {
